4ae954ca854b1f8b5f85fd1e491e6417cd831ad5
tomeido <tomeido@naver.com>
2023-03-09 16:32:01 +0900

    AdvancedSimplify test complete on Helix Toolkit

---
+++ HelixPlayground/selftrim.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using SharpDX;
+using System.Collections.Generic;
+
+namespace HelixPlayground
+{
+    
+    public partial class MainWindow
+    {
+        List<Vector3> p = new List<Vector3>();
+        List<Vector3> OutPt = new List<Vector3>();
+
+        public List<Vector3> SelfTrim(List<Vector3> x)
+        {
+
+            OutPt = new List<Vector3>();
+            
+            p = x;
+            int N = x.Count - 2;
+
+            Vector3 selfTrimmedPtA = new Vector3();
+            Vector3 selfTrimmedPtB = new Vector3();
+            Vector3 selfTrimmedPtC = new Vector3();
+
+            for (int i = 0; i < N; i++)
+            {
+                Vector3 aPt = p[i];
+                Vector3 bPt = p[i + 1];
+                Vector3 cPt = p[i + 2];
+
+                float aPtX = p[i].X;
+                float aPtY = p[i].Y;
+                float aPtZ = p[i].Z;
+                float bPtX = p[i + 1].X;
+                float bPtY = p[i + 1].Y;
+                float bPtZ = p[i + 1].Z;
+                float cPtX = p[i + 2].X;
+                float cPtY = p[i + 2].Y;
+                float cPtZ = p[i + 2].Z;
+
+                if (i == N - 2)
+                {
+                    if ((aPtX == bPtX && bPtX == cPtX && aPtY == bPtY && bPtY == cPtY)
+                      || (aPtY == bPtY && bPtY == cPtY && aPtZ == bPtZ && bPtZ == cPtZ)
+                      || (aPtZ == bPtZ && bPtZ == cPtZ && aPtX == bPtX && bPtX == cPtX))
+                    {
+                        Vector3 dPt = p[i + 3];
+
+                        selfTrimmedPtA = new Vector3(aPtX, aPtY, aPtZ);
+                        selfTrimmedPtC = new Vector3(cPtX, cPtY, cPtZ);
+
+                        OutPt.Add(selfTrimmedPtA);
+                        OutPt.Add(selfTrimmedPtC);
+                        OutPt.Add(dPt);
+
+                        i++;
+                    }
+                    else
+                    {
+                        selfTrimmedPtA = new Vector3(aPtX, aPtY, aPtZ);
+
+                        OutPt.Add(selfTrimmedPtA);
+                    }
+                }
+                else
+                {
+                    if (i == N - 1)
+                    {
+                        if ((aPtX == bPtX && bPtX == cPtX && aPtY == bPtY && bPtY == cPtY)
+                          || (aPtY == bPtY && bPtY == cPtY && aPtZ == bPtZ && bPtZ == cPtZ)
+                          || (aPtZ == bPtZ && bPtZ == cPtZ && aPtX == bPtX && bPtX == cPtX))
+                        {
+                            selfTrimmedPtA = new Vector3(aPtX, aPtY, aPtZ);
+                            selfTrimmedPtC = new Vector3(cPtX, cPtY, cPtZ);
+
+                            OutPt.Add(selfTrimmedPtA);
+                            OutPt.Add(selfTrimmedPtC);
+                        }
+                        else
+                        {
+                            selfTrimmedPtA = new Vector3(aPtX, aPtY, aPtZ);
+                            selfTrimmedPtB = new Vector3(bPtX, bPtY, bPtZ);
+                            selfTrimmedPtC = new Vector3(cPtX, cPtY, cPtZ);
+
+                            OutPt.Add(selfTrimmedPtA);
+                            OutPt.Add(selfTrimmedPtB);
+                            OutPt.Add(selfTrimmedPtC);
+                        }
+
+                    }
+                    else
+                    {
+                        if ((aPtX == bPtX && bPtX == cPtX && aPtY == bPtY && bPtY == cPtY)
+                          || (aPtY == bPtY && bPtY == cPtY && aPtZ == bPtZ && bPtZ == cPtZ)
+                          || (aPtZ == bPtZ && bPtZ == cPtZ && aPtX == bPtX && bPtX == cPtX))
+                        {
+                            selfTrimmedPtA = new Vector3(aPtX, aPtY, aPtZ);
+
+                            OutPt.Add(selfTrimmedPtA);
+
+                            i++;
+                        }
+                        else
+                        {
+                            selfTrimmedPtA = new Vector3(aPtX, aPtY, aPtZ);
+
+                            OutPt.Add(selfTrimmedPtA);
+                        }
+                    }
+                }
+            }
+            x = OutPt;
+            originalPolyline = x;
+            return x;
+        }
+    }
+}
---
+++ HelixPlayground/simplify.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using SharpDX;
+using System.Collections.Generic;
+
+namespace HelixPlayground
+{
+    public partial class MainWindow
+    {
+        List<Vector3> originalPolyline = new List<Vector3>();
+        List<Vector3> continuitedPolyline = new List<Vector3>();
+
+        public List<Vector3> Discontinuity(List<Vector3> originalPolylineVectors)
+        {
+            originalPolyline = originalPolylineVectors;
+            
+            continuitedPolyline.Add(originalPolyline[0]);
+
+            for(int i = 1; i < originalPolyline.Count()-1; i++)
+            {
+                //continuitedPolyline.Add(originalPolyline[i]);
+                if ((originalPolyline[i-1] - originalPolyline[i]).Length() 
+                    + (originalPolyline[i] - originalPolyline[i+1]).Length()
+                    != (originalPolyline[i-1] - originalPolyline[i + 1]).Length())
+                {
+                    continuitedPolyline.Add(originalPolyline[i]);
+                }
+            }
+
+            continuitedPolyline.Add(originalPolyline[originalPolyline.Count()-1]);
+            originalPolylineVectors = continuitedPolyline;
+            return originalPolylineVectors;
+        }
+
+
+
+        public void InitoriginalPolyline()
+        {
+            originalPolyline.Add(new Vector3(-11f, -16f, 0f));
+            originalPolyline.Add(new Vector3(-11f, -10f, 0f));
+            originalPolyline.Add(new Vector3(-11f, -3f, 0f));
+            originalPolyline.Add(new Vector3(-11f, 4f, 0f));
+            originalPolyline.Add(new Vector3(-11f, 12f, 0f));
+            originalPolyline.Add(new Vector3(-11f, 19f, 0f));
+            originalPolyline.Add(new Vector3(9f, 19f, 0f));
+            originalPolyline.Add(new Vector3(9f, 19f, 4f));
+            originalPolyline.Add(new Vector3(9f, 19f, 7f));
+            originalPolyline.Add(new Vector3(9f, 19f, 12f));
+            originalPolyline.Add(new Vector3(9f, 19f, 17f));
+            originalPolyline.Add(new Vector3(9f, -12f, 17f));
+            originalPolyline.Add(new Vector3(9f, 5f, 17f));
+            originalPolyline.Add(new Vector3(18f, 5f, 17f));
+            originalPolyline.Add(new Vector3(18f, 5f, 22f));
+            originalPolyline.Add(new Vector3(18f, 5f, 26f));
+            originalPolyline.Add(new Vector3(18f, 5f, 31f));
+            originalPolyline.Add(new Vector3(4f, 5f, 31f));
+            originalPolyline.Add(new Vector3(-6f, 5f, 31f));
+            originalPolyline.Add(new Vector3(-14f, 5f, 31f));
+            originalPolyline.Add(new Vector3(-14f, 4f, 31f));
+            originalPolyline.Add(new Vector3(-14f, 1f, 31f));
+            originalPolyline.Add(new Vector3(-14f, -9f, 31f));
+        }
+    }
+}
+
+
